5932aaf409c721f033f1c4cfb5d387aaa308e638
Connor Irvine <connor.irvine@hmrc.digital.gov.uk>
2018-01-22 12:09:01 +0000

    Added the question / conditional expression

--- src/main/scala/App/GreeterApplication.scala
+++ src/main/scala/App/GreeterApplication.scala
@@ -4,7 +4,15 @@ import scala.io.StdIn
 
 
 object GreeterApplication extends App {
-    def greet(name : String) :Unit = println("Hello " + name)
-    val name = StdIn.readLine("What is your name? ")
+  def greet(name: String): Unit = {
+    if (name == "Michael") {
+      println("Clear off you mug")
+    } else {
+      println(s"Hello $name")
+    }
+  }
+
+  val name = StdIn.readLine("What is your name? ")
+
   greet(name)
 }
